f1068fa5b6ed6b6c00441dba6fa6f618434b7c42
José F. Romaniello <jfromaniello@gmail.com>
2016-05-03 12:59:53 -0300

    add noop handler to tls errors

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var tls = require('tls');
 var inject = require('reconnect-core');
 var slice = [].slice;
+var noop = function () {};
 
 module.exports = inject(function(){
 	var args = slice.call(arguments);
 	var cleartextStream = tls.connect.apply(tls, args);
+
 	cleartextStream.on('secureConnect', function(){
 		if(cleartextStream.authorized){
 			cleartextStream.emit('connect');
 		}else{
 			cleartextStream.emit('error', cleartextStream.authorizationError);
 		}
-	});
+	}).on('error', noop);
+
 	return cleartextStream;
 });
